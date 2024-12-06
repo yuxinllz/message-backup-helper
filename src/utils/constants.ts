27c70337a9af876c4c1815ea4caeb47fe4c4615f
yuxinllz <89430012+yuxinllz@users.noreply.github.com>
2024-12-06 20:49:04 +1030

    Delete src directory

--- src/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-export const iosFilesPredefinedPathTree = {
-  contacts: ['Library', 'AddressBook', 'AddressBook.sqlitedb'],
-  messages: ['Library', 'SMS', 'sms.db']
-};
-
-export const iosFilesPredefinedPath = [
-  ['Library', 'AddressBook', 'AddressBook.sqlitedb'],
-  ['Library', 'SMS', 'sms.db']
-];
